ac7c16594d3bf1599da75e7b955a1dff6b4d7e4a
Radoslav Ivanov <radogi001@gmail.com>
2022-08-08 15:50:36 +0300

    added exercises from the LAB

---
+++ 01.ADO.NET/Exercises/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _02_VillainNames
+{
+    public static class Config
+    {
+        public const string ConnectionString = 
+            @"Server=DESKTOP-ONH52Q6\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;";
+
+    }
+}
