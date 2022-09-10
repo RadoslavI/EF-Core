0baab484cd07bc02b2d1d0d74cd4282bae6b9ec2
Radoslav Ivanov <radogi001@gmail.com>
2022-09-10 21:58:54 +0300

    initial commit

---
+++ 10.Exam Preperation 1/SoftJail/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-ONH52Q6\SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
+    }
+}
